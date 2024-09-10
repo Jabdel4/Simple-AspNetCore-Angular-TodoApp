abca1ef8cdd85b3e86f353cf9200563148f24462
Darrel Joris <joris.alima@agilion.ca>
2024-09-10 02:30:48 +0100

    Updated app module

--- TodoAppClient/src/app/app.module.ts
+++ TodoAppClient/src/app/app.module.ts
@@ -5,6 +5,7 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 
 import { HttpClientModule } from '@angular/common/http';
+import { FormsModule } from '@angular/forms';
 import { TodoComponent } from './components/todo/todo.component';
 
 @NgModule({
@@ -15,7 +16,8 @@ import { TodoComponent } from './components/todo/todo.component';
   imports: [
     BrowserModule,
     AppRoutingModule,
-    HttpClientModule
+    HttpClientModule,
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
